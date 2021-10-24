1f54d58fafc8de7c18025d121de40b100897d21a
Bart-15 <brat.tabusao@gmail.com>
2021-10-24 18:07:35 +0800

    Preparing

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,6 @@
+module.exports = {
+    MONGO_URI:'mongodb+srv://book:bookdirectory@bookdirectory.za8s2.mongodb.net/book-directory?retryWrites=true&w=majority'
+}
 
 if(process.env.NODE_ENV === 'production') {
     module.exports = require('./keys_prod')
--- server.js
+++ server.js
@@ -20,12 +20,11 @@ app.use(bodyParser.json());
 // Server static if in prod
 if(process.env.NODE_ENV === 'production'){
     // Set static folder
-    app.use(express.static('client/build'))
-
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+     // Set static folder
+     app.use(express.static('./client/build'))
+     app.get('*', (req, res) => {
+         res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+     })
 }
 
 
