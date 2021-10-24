2819e9587aa41157e500cfaa4b3ede40bedfa07b
Bart-15 <brat.tabusao@gmail.com>
2021-10-24 18:39:38 +0800

    Preparings

--- server.js
+++ server.js
@@ -17,21 +17,21 @@ parameterLimit: 100000}));
 app.use(bodyParser.json());
 
 
-// Server static if in prod
-if(process.env.NODE_ENV){
-    // Set static folder
-     // Set static folder
-     app.use(express.static('/client/build'))
-     app.get('*', (req, res) => {
-         res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-     })
-}
 
 
 
 
 app.use(book)
 
+// Server static if in prod
+if(process.env.NODE_ENV){
+    // Set static folder
+     // Set static folder
+     app.use(express.static('./client/build'))
+     app.get('*', (req, res) => {
+         res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+     })
+}
 app.listen(port, () => {
     console.log('listening on port ' + port);
 })
